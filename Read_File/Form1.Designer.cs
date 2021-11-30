41530baae5fda60357f58d334f86d1e955527d70
Long Huynh <hocap3508@gmail.com>
2021-11-30 19:43:53 +0700

    Add Read File

---
+++ Read_File/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Read_File
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button_text = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(50, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(481, 410);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button_text
+            // 
+            this.button_text.Location = new System.Drawing.Point(594, 47);
+            this.button_text.Name = "button_text";
+            this.button_text.Size = new System.Drawing.Size(94, 29);
+            this.button_text.TabIndex = 1;
+            this.button_text.Text = "Read file";
+            this.button_text.UseVisualStyleBackColor = true;
+            this.button_text.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_text);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button_text;
+    }
+}
+
---
+++ Read_File/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+using GemBox.Document;
+namespace Read_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //public static string readword(string path)
+        //{
+
+        //}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string path = @"D:\Chuyên đề CNPM\Midterm\text.docx";
+            //StreamReader stream = new StreamReader(path);
+            //string filedata = stream.ReadToEnd();
+            //richTextBox1.Text = filedata.ToString();
+            //stream.Close();
+
+            OpenFileDialog filedialog = new OpenFileDialog();
+            filedialog.ShowDialog();
+            string filedata = filedialog.FileName.ToString();
+            ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+
+            //// Load Word document from file's path.
+            var document = DocumentModel.Load(filedata);
+
+            // Get Word document's plain text.
+            string text = document.Content.ToString();
+            richTextBox1.Text = text;
+
+            //Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+            //Document doc = app.Documents.Open(filedata);
+            //string data = doc.Content.Text;
+            //richTextBox1.Text = filedata;
+            //app.Quit();
+
+            //ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+
+            ////// Load Word document from file's path.
+            //var document = DocumentModel.Load(@"D:\Chuyên đề CNPM\Midterm\text.docx");
+
+            //// Get Word document's plain text.
+            //string text = document.Content.ToString();
+            //richTextBox1.Text = text;
+
+
+
+
+
+
+
+        }
+    }
+}
