10f8ad0323cd8e908d81a7314eae537456f38672
Long Huynh <hocap3508@gmail.com>
2021-11-30 23:11:45 +0700

    Add Full Function

--- Read_File/Form1.Designer.cs
+++ Read_File/Form1.Designer.cs
@@ -31,35 +31,59 @@ namespace Read_File
         {
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button_text = new System.Windows.Forms.Button();
+            this.richTextBox_Path = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(50, 28);
+            this.richTextBox1.Location = new System.Drawing.Point(41, 149);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(481, 410);
+            this.richTextBox1.Size = new System.Drawing.Size(923, 410);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
             // button_text
             // 
-            this.button_text.Location = new System.Drawing.Point(594, 47);
+            this.button_text.Location = new System.Drawing.Point(796, 53);
             this.button_text.Name = "button_text";
-            this.button_text.Size = new System.Drawing.Size(94, 29);
+            this.button_text.Size = new System.Drawing.Size(168, 41);
             this.button_text.TabIndex = 1;
             this.button_text.Text = "Read file";
             this.button_text.UseVisualStyleBackColor = true;
             this.button_text.Click += new System.EventHandler(this.button1_Click);
             // 
+            // richTextBox_Path
+            // 
+            this.richTextBox_Path.Location = new System.Drawing.Point(41, 53);
+            this.richTextBox_Path.Name = "richTextBox_Path";
+            this.richTextBox_Path.Size = new System.Drawing.Size(721, 41);
+            this.richTextBox_Path.TabIndex = 2;
+            this.richTextBox_Path.Text = "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(41, 149);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(923, 410);
+            this.dataGridView1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1008, 571);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.richTextBox_Path);
             this.Controls.Add(this.button_text);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -68,6 +92,8 @@ namespace Read_File
 
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button_text;
+        private System.Windows.Forms.RichTextBox richTextBox_Path;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Read_File/Form1.cs
+++ Read_File/Form1.cs
@@ -9,15 +9,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.Office.Interop.Word;
 using GemBox.Document;
+using ExcelDataReader;
+using PdfiumViewer;
+
 namespace Read_File
 {
     public partial class Form1 : Form
     {
+        PdfiumViewer.PdfViewer pdf;
+
         public Form1()
         {
             InitializeComponent();
+            pdf = new PdfViewer();
+            this.Controls.Add(pdf);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -25,50 +31,97 @@ namespace Read_File
 
         }
 
-        //public static string readword(string path)
-        //{
-
-        //}
-
-        private void button1_Click(object sender, EventArgs e)
+        public void readText(string path)
         {
-            //string path = @"D:\Chuyên đề CNPM\Midterm\text.docx";
-            //StreamReader stream = new StreamReader(path);
-            //string filedata = stream.ReadToEnd();
-            //richTextBox1.Text = filedata.ToString();
-            //stream.Close();
+            richTextBox1.Text = File.ReadAllText(path);
+        }
 
-            OpenFileDialog filedialog = new OpenFileDialog();
-            filedialog.ShowDialog();
-            string filedata = filedialog.FileName.ToString();
+        public void readWord(string path)
+        {
             ComponentInfo.SetLicense("FREE-LIMITED-KEY");
-
-            //// Load Word document from file's path.
-            var document = DocumentModel.Load(filedata);
+            var document = DocumentModel.Load(path);
 
             // Get Word document's plain text.
             string text = document.Content.ToString();
             richTextBox1.Text = text;
+        }
 
-            //Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
-            //Document doc = app.Documents.Open(filedata);
-            //string data = doc.Content.Text;
-            //richTextBox1.Text = filedata;
-            //app.Quit();
+       
 
-            //ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+        public void readExcel(string path)
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
-            ////// Load Word document from file's path.
-            //var document = DocumentModel.Load(@"D:\Chuyên đề CNPM\Midterm\text.docx");
+            var stream = File.Open(path, FileMode.Open, FileAccess.Read);
+            var reader = ExcelReaderFactory.CreateReader(stream);
+            var result = reader.AsDataSet();
+            var tables = result.Tables.Cast<DataTable>();
+            foreach(DataTable table in tables)
+            {
+                dataGridView1.DataSource = table;
+            }
+        }
 
-            //// Get Word document's plain text.
-            //string text = document.Content.ToString();
-            //richTextBox1.Text = text;
+        public void readPDF(string path)
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+            var stream = new MemoryStream(bytes);
+            PdfDocument pdfDocument = PdfDocument.Load(stream);
+            pdf.Document = pdfDocument;
+        }
+
+        public void textBoxShow()
+        {
+            richTextBox1.Show();
+            dataGridView1.Hide();
+        }
+
+        public void gridViewShow()
+        {
+            richTextBox1.Hide();
+            dataGridView1.Show();
+        }
+
+        public void hiden()
+        {
+            richTextBox1.Hide();
+            dataGridView1.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog filedialog = new OpenFileDialog();
+            filedialog.Filter = "File|*.txt;*.doc;*.docx;*.xlsx;*.xlsm;*.pdf|All Files (*.*)|*.*";
+            if (filedialog.ShowDialog() == DialogResult.OK)
+            {
+                hiden();
+                richTextBox_Path.Text = filedialog.FileName;
+                if (filedialog.FileName.EndsWith("txt"))
+                {
+                    textBoxShow();
+                    readText(filedialog.FileName);
+                }
+                else if (filedialog.FileName.EndsWith("docx") || filedialog.FileName.EndsWith("doc"))
+                {
+                    textBoxShow();
+                    readWord(filedialog.FileName);
+                }
+                else if (filedialog.FileName.EndsWith("xlsx") || filedialog.FileName.EndsWith("xlsm"))
+                {
+                    gridViewShow();
+                    readExcel(filedialog.FileName);
+                    
+                }
+                else if (filedialog.FileName.EndsWith("pdf"))
+                {
+                    hiden();
+                    richTextBox_Path.Hide();
+                    readPDF(filedialog.FileName);
+
+                }
 
 
-
-
-
+            }
 
 
         }
